fb56bbb16ff5b4c0f950a1e34c1092ff3bd0a3a8
Iga Narendra <iga.narendra@gmail.com>
2022-04-18 17:40:19 +0000

    add 2d observation plane

--- sim/distance_model.py
+++ sim/distance_model.py
@@ -1,13 +1,52 @@
-# Initialise
+import random
+import math
 
-n_station=3
+def distance(veh,station):
+    x1 = veh[0]
+    y1 = veh[1]
+    x2 = station[0]
+    y2 = station[1] 
+    result = math.sqrt(math.pow((x1-x2),2) + math.pow((y1-y2),2))
+    return result
+
+# Initialise
+n_station=int(input("Stations: "))
 stations = [[]]
 
 # base stations, and another seperated by 200m
-stations[0]=(100,71) 
+stations[0]=(100,71)
+#stations.append((stations[0][0]+200*1,stations[0][1]))
 print('Base stations coordiates: ')
-print(stations[0])
-for a in range(1,n_station-1):
-    stations[a]=(stations[0][0]+200*a,stations[0][1])
-    print(stations[a])
+for a in range(1,n_station):
+    stations.append((stations[0][0]+(200*a),stations[0][1]))
 
+print(stations)
+
+print()
+# configure vehicles and motorway
+n_vehicles = int(input("Vehicles: "))
+n_rows = int(input("Motorway Lanes: "))
+row_width = float(input("Per Lane Width: "))
+vehicle_width = row_width/2
+x_limit = int(input("X limit: "))
+
+vehicles = []
+
+print("vehicles coordinates:")
+# generate random x and y value for vehicles
+for a in range(n_vehicles):
+    x = round(random.random()*x_limit)
+    y = abs(round(random.random()*n_rows-1))
+    y = (y*row_width)+vehicle_width
+    vehicles.append((x,y))
+
+print(vehicles)
+
+print()
+print("distances: ")
+# count each vehicle distances to stations
+for a in range(n_vehicles):
+    print("For vehicle ",vehicles[a])
+    for b in range(n_station):
+        print("with station ",stations[b],": ",distance(vehicles[a],stations[b]))
+    print()
